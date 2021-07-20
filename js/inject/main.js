bfab7df5aaccfb90aa8be5343d1f49dddca1ee3f
gastbob40 <gastbob40@gmail.com>
2021-07-20 17:37:40 +0200

    feat (src): reformat code & style notification

---
+++ js/inject/main.js
@@ -0,0 +1,129 @@
+function notify(text) {
+    const notificationTime = 3;
+
+    const style = document.createElement("style");
+
+    document.head.appendChild(style);
+    style.textContent = ".party-notification {\n" +
+        "    border-radius: 4px;\n" +
+        "    position: fixed;\n" +
+        "    bottom: 6px;\n" +
+        "    right: 16px;\n" +
+        "    background: #191919;\n" +
+        "    color: white;\n" +
+        "    opacity: 0;\n" +
+        "    font-size: 1.2em;\n" +
+        "    transition: 0.3s linear;\n" +
+        "    z-index: 99;\n" +
+        "}\n" +
+        ".party-message {\n" +
+        "    padding: 16px 32px;\n" +
+        "}\n" +
+        ".party-progress {\n" +
+        "    width: 0;\n" +
+        "    height: 2px;\n" +
+        "    background: #ef3e3a;\n" +
+        "    background: #ef3e3a;\n" +
+        "    border-radius: 4px;\n" +
+        `    transition: ${notificationTime}s linear;\n` +
+        "}\n";
+
+
+    const body = document.querySelector("body");
+    const notification = document.createElement("div");
+    const message = document.createElement("div");
+    const progress = document.createElement("div");
+
+    progress.classList.add("party-progress");
+    message.classList.add("party-message");
+    message.innerText = text;
+    notification.classList.add("party-notification")
+
+    body.appendChild(notification);
+    notification.appendChild(message);
+    notification.appendChild(progress);
+
+
+    setTimeout(() => {
+        notification.style.transform = "translateY(-10px)";
+        notification.style.opacity = "100%";
+        progress.style.width = "100%";
+    }, 10);
+
+    setTimeout(() => {
+        notification.style.transform = "";
+        notification.style.opacity = "0";
+    }, notificationTime * 1000);
+
+    setTimeout(() => {
+        notification.parentNode.removeChild(notification);
+    }, notificationTime * 1000 + 300);
+
+    console.log(message);
+}
+
+function getVideo() {
+
+
+    const video = document.querySelector("video");
+
+    if (video == null) {
+        notify("No Video found");
+        return;
+    }
+
+    let socket = null;
+    try {
+        socket = new WebSocket('wss://video-party.gastbob40.ovh/', 'echo-protocol')
+    } catch (e) {
+        console.log(e);
+        return;
+    }
+
+
+    socket.onopen = function (event) {
+        let client = this;
+        console.log("Connexion établie.");
+
+        this.onclose = function (event) {
+            alert("Connection closed from server")
+        };
+
+
+        this.onmessage = function (event) {
+            const data = JSON.parse(event.data);
+
+            if (Math.abs(video.currentTime - data['time']) > 0.1)
+                video.currentTime = data['time'];
+
+            if (data['command'] === "pause") {
+                video.pause();
+            } else if (data['command'] === "play") {
+                video.play();
+            }
+        };
+
+        video.addEventListener('pause', function () {
+            client.send(JSON.stringify({
+                "command": "pause",
+                "time": video.currentTime
+            }))
+        })
+
+        video.addEventListener('play', function () {
+            client.send(JSON.stringify({
+                "command": "play",
+                "time": video.currentTime
+            }))
+        })
+
+        video.addEventListener('seeked', function () {
+            client.send(JSON.stringify({
+                "command": "seeked",
+                "time": video.currentTime
+            }))
+        })
+    };
+}
+
+getVideo();
--- popup.js
+++ popup.js
@@ -1,96 +1,11 @@
-function getVideo() {
-    const video = document.querySelector("video");
-
-    if (video == null) {
-        alert("No Video found");
-        return;
-    }
-
-    let socket = null;
-    try {
-        // socket = new WebSocket('wss://51.77.132.51:8080/', 'echo-protocol');
-        socket = new WebSocket('wss://video-party.gastbob40.ovh/', 'echo-protocol')
-    } catch (e) {
-        console.log(e);
-        return;
-    }
-
-
-    socket.onopen = function (event) {
-        let client = this;
-        console.log("Connexion établie.");
-
-        this.onclose = function (event) {
-            alert("Connection closed from server")
-        };
-
-
-        this.onmessage = function (event) {
-            const data = JSON.parse(event.data);
-
-            if (Math.abs(video.currentTime - data['time']) > 0.1)
-                video.currentTime = data['time'];
-
-            if (data['command'] === "pause") {
-                video.pause();
-            } else if (data['command'] === "play") {
-                video.play();
-            }
-        };
-
-        video.addEventListener('pause', function () {
-            client.send(JSON.stringify({
-                "command": "pause",
-                "time": video.currentTime
-            }))
-        })
-
-        video.addEventListener('play', function () {
-            client.send(JSON.stringify({
-                "command": "play",
-                "time": video.currentTime
-            }))
-        })
-
-        video.addEventListener('seeked', function () {
-            client.send(JSON.stringify({
-                "command": "seeked",
-                "time": video.currentTime
-            }))
-        })
-    };
-}
-
 createParty.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
 
     chrome.scripting.executeScript({
         target: {tabId: tab.id},
-        function: getVideo,
+        files: ['js/inject/main.js'],
     });
 });
-
-function notify() {
-    const message = "az"
-
-    const body = document.querySelector("body");
-    const notification = document.createElement("div");
-    notification.classList.add("party-notification")
-
-    body.appendChild(notification);
-
-    console.log(message);
-}
-
-test.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        function: notify(),
-    });
-});
-
 
 /// Events about page changing
 joinParty.addEventListener('click', async () => {
