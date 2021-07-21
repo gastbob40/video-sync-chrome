a92409543a5e6516e45c42d10b4b0825be3f8995
gastbob40 <gastbob40@gmail.com>
2021-07-22 00:07:41 +0200

    feat (src): link for popup

--- popup.js
+++ popup.js
@@ -53,6 +53,32 @@ accessCode.addEventListener('click', async () => {
     chrome.tabs.sendMessage(tab.id, {kind: 'notify', message: "The code has been copied into your clipboard"});
 })
 
+codeInput.addEventListener('change', async (value) => {
+
+        fetch(baseUrl + "rooms/" + value.target.value + '/', {
+            method: "GET",
+        }).then(async res => {
+            let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+            if (res.status === 200) {
+                res.json().then(room => {
+                    document.querySelector("#join").style.display = 'none';
+                    document.querySelector("#party").style.display = 'block';
+                    document.querySelector("#accessCode").innerHTML = room.code;
+
+                    if (tab.url !== room.pageUrl) {
+                        chrome.tabs.update(tab.id, {url: room.pageUrl});
+                    }
+                    chrome.storage.sync.set({room: room});
+                    chrome.tabs.sendMessage(tab.id, {kind: 'sync', code: room.code});
+                });
+            } else {
+                chrome.tabs.sendMessage(tab.id, {kind: 'notify', message: "Cannot find room with this code"});
+            }
+        });
+    }
+)
+
+
 chrome.storage.sync.get(['room'], function (result) {
     if (result.room !== undefined) {
         document.querySelector("#home").style.display = 'none';
